24a3be1f70c6d1afee623b08ec05b83202969280
Jeffrey Li <dr.cheddar@outlook.com>
2022-03-12 13:35:50 -0500

    doc: Mapped Entries function

--- src/utils/core.ts
+++ src/utils/core.ts
@@ -11,6 +11,26 @@ export const deepRequired = <T extends Object>(obj: T): DeepRequired<T> =>
 		.filter(([, v]) => typeof v !== 'undefined')
 		.map(([k, v]) => [k, v instanceof Object && !Array.isArray(v) ? deepRequired(v) : v])) as DeepRequired<T>;
 
+// Maps the entries of an object with the correct typings.
+
 export const MapEntries = <T extends Object>(obj: T) =>
 	<V>(f: (k: T[keyof T]) => V) =>
 		Object.fromEntries(TypedEntries(obj).map(([k, v]) => [k, f(v)] as [keyof T, V])) as { [K in keyof T]: V };
+
+/**
+ * Example:
+	const mapTest = {
+		1: 1,
+		2: 2,
+	} as const;
+
+	const mapper = <S extends number>(i: S) => `${i}` as `${S}`;
+	const mappedTest = MapEntries(mapTest)(mapper);
+
+Result Typing:
+
+	const mappedTest: {
+			readonly 1 : "1" | "2",
+			readonly 2 : "1" | "2",
+	}
+ */
